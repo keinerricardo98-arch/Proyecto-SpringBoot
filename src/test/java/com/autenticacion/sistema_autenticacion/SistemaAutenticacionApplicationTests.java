4e3d65b2c21cada2a4101b8415d54a58056bcf7b
keinerricardo98-arch <keinerricardo98@gmail.com>
2025-11-25 00:49:53 -0500

    nuevo repositorio

---
+++ src/test/java/com/autenticacion/sistema_autenticacion/SistemaAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autenticacion.sistema_autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
