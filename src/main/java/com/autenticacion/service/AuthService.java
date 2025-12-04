6b2222861c7cff2bf08b44c4c221aaa223adb4d0
keinerricardo98-arch <keinerricardo98@gmail.com>
2025-12-04 17:39:53 -0500

    Repositorio modificado

--- src/main/java/com/autenticacion/service/AuthService.java
+++ src/main/java/com/autenticacion/service/AuthService.java
@@ -103,4 +103,4 @@ public class AuthService {
         }
         return request.getRemoteAddr();
     }
-}}
+}
--- src/main/java/com/autenticacion/service/UserService.java
+++ src/main/java/com/autenticacion/service/UserService.java
@@ -28,4 +28,3 @@ public class UserService {
         return userRepository.save(user);
     }
 }
-}
